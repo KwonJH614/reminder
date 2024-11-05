4c09800c6209ab1d4d38475a627d6ae6592343b6
madogkwon <a93081077a@gmail.com>
2024-11-05 23:40:04 +0900

    change method updateMail(post -> put)

--- src/main/java/com/example/reminder/ReminderApplication.java
+++ src/main/java/com/example/reminder/ReminderApplication.java
@@ -2,7 +2,10 @@ package com.example.reminder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 
 @SpringBootApplication
 @EnableScheduling
@@ -12,4 +15,9 @@ public class ReminderApplication {
 		SpringApplication.run(ReminderApplication.class, args);
 	}
 
+	@Bean
+	public FilterRegistrationBean<HiddenHttpMethodFilter> hiddenHttpMethodFilter() {
+		FilterRegistrationBean<HiddenHttpMethodFilter> filterRegistrationBean = new FilterRegistrationBean<>(new HiddenHttpMethodFilter());
+		return filterRegistrationBean;
+	}
 }
