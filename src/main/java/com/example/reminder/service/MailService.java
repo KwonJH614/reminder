c78ec176a5d65c670a0e045f6b582ea710747e95
madogkwon <a93081077a@gmail.com>
2024-11-05 11:07:10 +0900

    add Service(sendMail)

--- src/main/java/com/example/reminder/service/MailService.java
+++ src/main/java/com/example/reminder/service/MailService.java
@@ -3,8 +3,11 @@ package com.example.reminder.service;
 import com.example.reminder.entity.Mail;
 import com.example.reminder.repository.MailRepository;
 import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMailMessage;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -58,6 +61,27 @@ public class MailService {
   }
 
   public void sendMail(Mail mail) throws MessagingException {
+    MimeMessage message = mailSender.createMimeMessage();
+    MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
 
+    helper.setTo(mail.getSendTo());
+    helper.setSubject("Reminder");
+
+    String htmlContent ="<html>"
+            + "<body style='font-family: Arial, sans-serif;'>"
+            + "<div style='background-color: #f3f4f6; padding: 20px; border-radius: 5px;'>"
+            + "  <h2 style='color: #4a90e2; text-align: center;'>" + mail.getTitle() + "</h2>"
+            + "  <p style='font-size: 16px; color: #333;'>안녕하세요!</p>"
+            + "  <p style='font-size: 14px; color: #555;'>"
+            + "    <strong>기념일 날짜:</strong> " + mail.getDate() + "<br>"
+            +  mail.getTitle() + " 을(를) 축하합니다! 특별한 날을 기념하세요."
+            + "  </p>"
+            + "</div>"
+            + "</body>"
+            + "</html>";
+
+    helper.setText(htmlContent, true);
+
+    mailSender.send(message);
   }
 }
